1b2cd3a2f505f41c58fffa2331ab004ebdf0d06f
Ubuntu <ubuntu@ip-172-31-36-222.ap-northeast-1.compute.internal>
2026-07-01 02:44:34 +0000

    Users_model

---
+++ db/migrate/20260701024105_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :email_address, null: false
+      t.string :password_digest, null: false
+      t.text :introduction
+      t.boolean :is_active, null: false, default: true
+
+      t.timestamps
+    end
+
+    add_index :users, :email_address, unique: true
+  end
+end
