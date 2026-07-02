702927e799b63d339fed21cc6ca67320f3bd0f16
Ubuntu <ubuntu@ip-172-31-36-222.ap-northeast-1.compute.internal>
2026-07-02 02:28:47 +0000

    model

---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
+
+  has_many_attached :images
+  
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :place_name, presence: true
+  validates :address, presence: true
+  validates :rating, presence: true, inclusion: { in: 1..5}
+end
