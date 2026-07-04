14cd16e0e1cabf31771d665372ea9ca5bd23ec34
Ubuntu <ubuntu@ip-172-31-36-222.ap-northeast-1.compute.internal>
2026-07-04 04:45:19 +0000

    test_resolve

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_07_02_022724) do
+ActiveRecord::Schema[8.0].define(version: 2026_07_03_075845) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,18 @@ ActiveRecord::Schema[8.0].define(version: 2026_07_02_022724) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "dogs", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name", null: false
+    t.string "breed", null: false
+    t.integer "size", null: false
+    t.date "birthday", null: false
+    t.integer "gender", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -66,5 +78,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_07_02_022724) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "dogs", "users"
   add_foreign_key "posts", "users"
 end
