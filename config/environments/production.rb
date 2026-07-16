7eb1e6aea3913cf412b14d3a6413fa419e491c5e
Ubuntu <ubuntu@ip-172-31-42-140.ap-northeast-1.compute.internal>
2026-07-16 03:09:52 +0000

    Fix production email delivery settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,10 +55,23 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
 
-  # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+config.action_mailer.default_url_options = {
+  host: "16.76.141.158",
+  protocol: "http"
+}
+
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_APP_PASSWORD"],
+  authentication: :plain,
+  enable_starttls_auto: true
+}
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
