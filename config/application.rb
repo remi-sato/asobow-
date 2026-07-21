7ce62f51869673d4f43b00e64cd97110740f6473
Ubuntu <ubuntu@ip-172-31-36-222.ap-northeast-1.compute.internal>
2026-07-21 08:37:04 +0000

    Polish community UI and layout

--- config/application.rb
+++ config/application.rb
@@ -23,9 +23,8 @@ module Asobow
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :utc
     config.active_support.use_solid_cache = false
   end
 end
